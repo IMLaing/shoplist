d8e70b5138c389f947d29bb3a732a6f5a05bb578
IMLaing <ianmlaing@gmail.com>
2016-06-17 18:53:59 -0400

    mentor was complaining

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-    console.log('ready!');
-    var $userinput = $('.submitItem');
+	console.log('ready!');
+	var $userinput = $('.submitItem');
 
-    $('.add').click(function() {
-    //	var newItem = $('.submitItem').val();
-	    $('.needList').prepend("<li>" + $userinput.val() +"</li> <button class='check'> CHECK </button><button class='remove'> REMOVE </button>");
-	        console.log('add');
-	    $userinput.val('');
-
-    }); 
-
+	$('.add').click(function() {
+	    if ($userinput.val() == ''){
+	    	alert('No item to add!');
+	    } else {
+	    	$('.needList').prepend("<li>" + $userinput.val() +
+	    		"</li> <button class='check'> CHECK </button> <button class='remove'> REMOVE </button>");
+	    	console.log('add');
+	    	$userinput.val('');
+	    }
+	});
 });
