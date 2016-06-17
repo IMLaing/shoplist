9359d4d1bd88e51dea517e05e5b9f9e0c2ea1a25
IMLaing <ianmlaing@gmail.com>
2016-06-17 18:39:25 -0400

    changed variable exe

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     console.log('ready!');
-var newItem = $('.submitItem').val();
+    var $userinput = $('.submitItem');
 
     $('.add').click(function() {
-    $('.needList').prepend("<li>" + $('.submitItem').val() +"</li> <button class='check'> CHECK </button>	<button class='remove'> REMOVE </button>");
-        console.log('add');
+    //	var newItem = $('.submitItem').val();
+	    $('.needList').prepend("<li>" + $userinput.val() +"</li> <button class='check'> CHECK </button><button class='remove'> REMOVE </button>");
+	        console.log('add');
+	    $userinput.val('');
 
     }); 
 
