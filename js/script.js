63d887551979dbf89b87f78849aeb625733e858d
IMLaing <ianmlaing@gmail.com>
2016-06-18 10:54:51 -0400

    added font-awesome, event.preventDefault()

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
 	console.log('ready!');
 	var $userinput = $('.submitItem');
 
-	$('.add').click(function() {
-	    if ($userinput.val() == ''){
+	$('#addForm').submit(function(event) {
+		event.preventDefault();
+		if ($userinput.val() == ''){
 	    	alert('No item to add!');
 	    } else {
 	    	$('.needList').prepend("<li>" + $userinput.val() +
