304292b8097ffdf2aff858a401dd8b4f3343caaf
IMLaing <ianmlaing@gmail.com>
2016-06-18 18:06:25 -0400

    working on show/hide function on the buttons

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
 $(document).ready(function(){
 	console.log('ready!');
 	var $userinput = $('.submitItem');
-
+	var $buttons = "<button class='check' id='checkID'><i class='fa fa-check'></i></button><button class='remove' id='removeID'><i class='fa fa-times'></i> </button><button class='add' id='addID'> <i class='fa fa-plus'></i> </button>";
+	
 	$('#addForm').submit(function(event) {
 		event.preventDefault();
 		if ($userinput.val() == ''){
 	    	alert('No item to add!');
 	    } else {
 	    	$('.needList').prepend("<div class='listItem'><li >" + $userinput.val() +
-	    		"</li> <button class='check'> <i class='fa fa-check'></i> </button> <button class='remove'> <i class='fa fa-times'></i> </button></div>");
-	    	console.log('add');
+	    		"</li>" + $buttons + "</div>");
+	    	console.log('add and hide addToList');
 	    	$userinput.val('');
-	    }
+	    	$('#addID').hide();
+	    };
 	});
 
 	$('ul').on('click', 'button.remove', function(){
 		console.log('clicked remove to erase');
 		$(this).parent().remove();
+
 	});
 
 	$('ul').on('click', 'button.check', function(){
 		console.log('clicked check to complete');
 		$(this).parent().appendTo('#finItem');
+		console.log('sent to finCart')
+		$('#addID').show(this);
+		console.log('#addID.show()')
+
+		
 	});
 
-	$('#finItem').on('click', '#addToList', function(){
+	$('#finItem').on('click', '#addID', function(){
 		$(this).parent().appendTo('.needList');
 		console.log('return to onList');
 	});
