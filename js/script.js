d1ecf134b06f4084c5bdc905eb44db4a5e1c63d2
IMLaing <ianmlaing@gmail.com>
2016-06-19 11:26:51 -0400

    sortable list - need return button to properly show() after the check button is pressed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 	console.log('ready!');
 	var $userinput = $('.submitItem');
-	var $buttons = "<button class='check' id='checkID'><i class='fa fa-check'></i></button><button class='remove' id='removeID'><i class='fa fa-times'></i> </button><button class='add' id='addID'> <i class='fa fa-plus'></i> </button>";
+	var $buttons = "<button class='add' id='addID'> <i class='fa fa-plus'></i> </button><button class='check' id='checkID'><i class='fa fa-check'></i></button><button class='remove' id='removeID'><i class='fa fa-times'></i> </button>";
 	
+//enter text to submit to list	
 	$('#addForm').submit(function(event) {
 		event.preventDefault();
 		if ($userinput.val() == ''){
 	    	alert('No item to add!');
 	    } else {
-	    	$('.needList').prepend("<div class='listItem'><li >" + $userinput.val() +
+	    	$('#needList').prepend("<div class='listItem'><li >" + $userinput.val() +
 	    		"</li>" + $buttons + "</div>");
 	    	console.log('add and hide addToList');
 	    	$userinput.val('');
@@ -16,25 +17,39 @@ $(document).ready(function(){
 	    };
 	});
 
+//remove item from any list
 	$('ul').on('click', 'button.remove', function(){
 		console.log('clicked remove to erase');
 		$(this).parent().remove();
-
 	});
 
+//move item to complete
 	$('ul').on('click', 'button.check', function(){
 		console.log('clicked check to complete');
-		$(this).parent().appendTo('#finItem');
-		console.log('sent to finCart')
-		$('#addID').show(this);
-		console.log('#addID.show()')
+		var toFin = $(this).parent();
+		$(this).remove();
+		$('#finItem').append(toFin);
+		console.log('sent to finCart');
+		$('#addID').show();
+		console.log('#addID.show()');
 
 		
 	});
 
 	$('#finItem').on('click', '#addID', function(){
-		$(this).parent().appendTo('.needList');
+		var reAdd = $(this).parent();
+		$(this).remove();
+		$('#needList').append(reAdd);
 		console.log('return to onList');
+		$(this).hide('#addID');
+		console.log('hiding #addID on return');
 	});
 
+	
+  $(function() {
+    	$('#needList').sortable({axis: 'y'});
+    	$('#needList').disableSelection();
+  	});
+
 });
+
